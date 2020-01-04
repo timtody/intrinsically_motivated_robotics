ee8a26c8effdc9bbb5d708db1be1a62a25528cb8
Julius Taylor <taylor@fias.uni-frankfurt.de>
2020-01-04 17:22:34 +0100

    Add agent template

---
+++ agents.py
@@ -0,0 +1,14 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class A2CAgent:
+    def __init__(self):
+        pass
+    
+    def get_action(self, state):
+        pass
+    
+    def train(self):
+        pass
