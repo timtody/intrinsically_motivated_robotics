8f251cb01ece08f9e991f2bf11a20e2abbea5b17
Julius Taylor <taylor@fias.uni-frankfurt.de>
2020-01-06 21:19:55 +0100

    Start agent

--- test.py
+++ test.py
@@ -1,48 +1,40 @@
+import random
+
 import torch
 import torch.optim as optim
 import torch.nn.functional as F
 
-import random
 from matplotlib import pyplot as plt
-from models import ForwardModule, InverseModule, shared_conv
+import seaborn as sns
+from models import ICModule
 
 torch.manual_seed(42)
+sns.set()
 
 h = w = 42
 n_actions = 4
 
-fmodule = ForwardModule(h, w)
-imodule = InverseModule(h, w, n_actions)
+icmodule = ICModule(h, w, n_actions)
 
-dummy_data = torch.randn(1, 1, 42, 42)
-dummy_data_2 = torch.randn(1, 1, 42, 42)
-dummy_data_3 = torch.randn(1, 1, 42, 42)
+STATES = [torch.randn(1, 1, 42, 42) for _ in range(7)]
 
 
-next_hidden_state = fmodule(dummy_data, torch.tensor([0]).float())
-predicted_action = imodule(dummy_data, dummy_data)
-
-optimizer = optim.Adagrad(fmodule.parameters(), lr=1e-4)
-
-print(shared_conv.state_dict()["conv1.bias"])
-print(fmodule.conv_base.state_dict()["conv1.bias"])
-
-states = [dummy_data, dummy_data_2, dummy_data_3]
+next_hidden_state = icmodule.next_state(STATES[0], torch.tensor([0]).float())
+predicted_action = icmodule.get_action(STATES[0], STATES[0])
+optimizer = optim.Adagrad(icmodule.parameters(), lr=1e-4)
 
 def get_action_and_target(states):
-    action = random.choice([0, 1, 2])
-    target_state = shared_conv(states[action])
+    action = random.choice([0, 1, 2, 3, 4, 5, 6])
+    target_state = icmodule.embed(states[action])
     action = torch.tensor([action]).float()
     return action, target_state
 
 losses = []
-for i in range(3000):
-    if i == 1000:
-        states = [states[-1]] + states[:-1]
-    if i == 2000:
-        states = [states[-1]] + states[:-1] 
-    action, target = get_action_and_target(states)
-    next_state = fmodule(states[int(action.item())], action)
+for i in range(50000):
+    if i % 5000 == 0:
+        STATES = [STATES[-1]] + STATES[:-1]
+    action, target = get_action_and_target(STATES)
+    next_state = icmodule.next_state(STATES[int(action.item())], action)
     loss = F.mse_loss(next_state, target)
     loss.backward()
     optimizer.step()
@@ -50,6 +42,4 @@ for i in range(3000):
     if i % 100 == 0: print(loss.item())
 
 plt.plot(losses)
-plt.savefig("losses.png")
-print(shared_conv.state_dict()["conv1.bias"])
-print(fmodule.conv_base.state_dict()["conv1.bias"])
+plt.show("losses.png")
