0e41ce1f8e6614071633e486358c25e9ca06eeea
Julius Taylor <taylor@fias.uni-frankfurt.de>
2020-01-06 22:08:48 +0100

    Start working on agent, implement get_action

--- agents.py
+++ agents.py
@@ -7,14 +7,35 @@ class A2CAgent:
     def __init__(self):
         self.policy = None
         self.opt = None
+        
+        self.rewards = []
+        self.entropies = []
+        self.logprobs = []
+        self.values = []
 
     def get_action(self, state):
-        pass
+        state = torch.tensor(state)
+        probs, value = self.policy(state)
+        c = torch.distributions.Categorical(probs)
+        action = c.sample()
+        self.entropies.append(c.entropy())
+        self.logprobs.append(c.log_prob(action))
+        self.values.append(value.squeeze())
 
     def train(self):
+        pass
+    
+    def _compute_loss(self):
         pass
 
     def set_policy_network(self, net):
         self.policy = net
         self.opt = optim.Adadelta(net.parameters())
+    
+    def _clear_buffers(self):
+        self.rewards = []
+        self.logprobs = []
+        self.values = []
+        self.entropies = []
+        self.is_done = False
     
