Fix bug in color gradient

main.py
@@ -6,6 +6,7 @@ from omegaconf import OmegaConf
 from models import ICModule
 from ppo_cont import PPO, Memory
 from wrappers import ObsWrapper
+from utils import ColorGradient
 
 cnf = OmegaConf.load("conf/conf.yaml")
 cnf.merge_with_cli()
@@ -23,28 +24,38 @@ icmodule = ICModule(obs_space, 1, action_dim)
 
 if cnf.wandb.use:
     wandb.init(project=cnf.wandb.project, name=cnf.wandb.name)
-    # wandb.watch(agent.policy, log="all")
-    wandb.watch(icmodule, log="parameters")
+    wandb.watch(agent.policy, log="all")
+    wandb.watch(icmodule._forward, log="all")
 
 state = env.reset()
 done = False
 timestep = 0
 gripper_positions = []
+
+# color gradients
+red = [220, 36, 36]
+blue = [74, 86, 157]
+gradient = ColorGradient(blue, red)
+
 for i in range(cnf.main.max_timesteps):
     timestep += 1
-    gripper_positions.append(state.gripper_pose[:3])
+    gripper_positions.append(np.array([*state.gripper_pose[:3], 
+                              *gradient.get(i / cnf.main.max_timesteps)]))
     action = agent.policy_old.act(state.get_low_dim_data(), memory)
     next_state, _, done, _ = env.step(action)
     # env.render()
     im_loss = icmodule.train_forward(state.get_low_dim_data(),
                                      next_state.get_low_dim_data(),
                                      action)
+    im_loss_processed = icmodule._process_loss(im_loss)
     if cnf.wandb.use:
         wandb.log({
-            "intrinsic reward": im_loss
+            "intrinsic reward raw": im_loss,
+            "intrinsic reward": im_loss_processed,
+            "reward std": icmodule.loss_buffer.get_std()
         })
     # IM loss = reward currently
-    reward = im_loss
+    reward = im_loss_processed
     memory.rewards.append(reward)
     memory.is_terminals.append(done)
     state = next_state

models.py
@@ -157,8 +157,7 @@ class ICModule(nn.Module):
         loss = F.mse_loss(next_state_embed_pred, next_state_embed_true)
         loss.backward()
         self.opt.step()
-        loss = self._process_loss(loss)
-        return loss.item()
+        return loss
 
     def _process_loss(self, loss):
         self.loss_buffer.push(loss)

ppo_cont.py
@@ -4,8 +4,8 @@ import torch.nn.functional as F
 from torch.distributions import MultivariateNormal
 
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
+# device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+device = torch.device("cpu")
 
 class Memory:
     def __init__(self):
@@ -76,7 +76,7 @@ class ActorCritic(nn.Module):
         memory.actions.append(action)
         memory.logprobs.append(dist.log_prob(action))
 
-        return action.clamp(-.2, .2).numpy()
+        return action.clamp(-.1, .1).numpy()
 
     def evaluate(self, state, action):
         locs, stds = self.action_layer_cont(state)