90cfa870bdfe2d75d1b9c432be35da95541cb371
Julius <julius@pop-os.localdomain>
2020-02-03 19:50:51 +0100

    Add mp functionality to counting script

---
+++ count_collisions_mp.py
@@ -0,0 +1,80 @@
+from env.environment import Env
+from algo.models import ICModule
+from conf import get_conf
+from algo.ppo_cont import PPO, Memory
+import pickle
+import plotly.graph_objects as go
+import numpy as np
+import torch
+from multiprocessing import Array, Process
+
+
+def run(rank, cnf, mode, results):
+    # random seed generation
+    np.random.seed()
+    torch.manual_seed(np.random.randint(999))
+
+    if mode == "notrain":
+        cnf.main.train = False
+    else:
+        cnf.main.train = True
+        cnf.env.state_size = mode
+    # re-init random seed per process
+    env = Env(cnf)
+    agent = PPO(env, **cnf.ppo)
+    memory = Memory()
+    icmodule = ICModule(env)
+    state = env.reset()
+    timestep = 0
+    n_collisions = 0
+    print(f"Starting mode {mode}, rank: {rank}")
+    for _ in range(cnf.main.max_timesteps):
+        timestep += 1
+        action = agent.policy_old.act(state.get(), memory)
+        next_state, _, done, info = env.step(action)
+        im_loss = icmodule.train_forward(state.get(), next_state.get(), action)
+        im_loss_processed = icmodule._process_loss(im_loss)
+        memory.rewards.append(im_loss_processed)
+        memory.is_terminals.append(done)
+        state = next_state
+        # count collisions (implicit coercion to int)
+        n_collisions += info["collided"]
+        # agent training
+        if timestep % cnf.main.train_each == 0 and cnf.main.train:
+            agent.update(memory)
+            memory.clear_memory()
+            timestep = 0
+    results[rank] = n_collisions
+    env.close()
+
+
+def run_mode_mp(mode, cnf):
+    processes = []
+    results = Array('d', range(cnf.mp.n_procs))
+    for rank in range(cnf.mp.n_procs):
+        p = Process(target=run, args=(rank, cnf, mode, results))
+        p.start()
+        processes.append(p)
+
+    for p in processes:
+        p.join()
+
+    return results[:]
+
+
+if __name__ == "__main__":
+    # get config setup
+    cnf = get_conf("conf/cnt_col.yaml")
+    results = []
+    state_modes = ["notrain", "notouch", "all"]
+    for mode in state_modes:
+        results.append(run_mode_mp(mode, cnf))
+    results = np.array(results)
+    print(results)
+    fig = go.Figure([
+        go.Bar(x=state_modes,
+               y=np.mean(results, axis=1),
+               error_y=dict(type='data', array=np.std(results, axis=1)))
+    ])
+    fig.write_html(f"data/{cnf.log.name}_result_mp.html")
+    fig.show()
--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 from env.environment import Env
 from conf import get_conf
 from utils import ReturnWindow
+from multiprocessing import Process, Array
+import numpy as np
 
 cnf = get_conf("conf/cnt_col.yaml")
 env = Env(cnf)
@@ -12,11 +14,27 @@ win = ReturnWindow(lookback=50)
 # for i in range(32):
 #     obs, *_, info = env.step([0, 0, 0, 0, 0, 1, -1])
 
-for i in range(10):
-    env.step([0, 0, 0, 0, 0, -1, 0])
+"""
+a = np.array([[2351, 236, 1090, 126, 8912], [6222, 7365,  120,  948, 5490], [5123, 3799,  3747, 1549, 1023]],)
 
-while not done:
-    timestep += 1
-    obs, *_, info = env.step([0, 1, 0, 0, 0, 0, 0])
-    relevant_forces = obs.get()[-15:]
-    win.update(relevant_forces)
+"""
+
+
+def run(rank, mode, cnf, results):
+    np.random.seed()
+    print("rank", rank, "writing results.")
+    results[rank] = np.random.randint(99)
+
+
+if __name__ == "__main__":
+    processes = []
+    results = Array('d', range(4))
+    for rank in range(4):
+        p = Process(target=run, args=(rank, "notrain", cnf, results))
+        p.start()
+        processes.append(p)
+
+    for p in processes:
+        p.join()
+
+    print(results[:])
