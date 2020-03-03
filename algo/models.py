List of changed: - changed running average of return to EMA - removed PPO reward normalization - introduced PPO advantage normalization - introduced bnorm into embedding network in icm - fixed faulty .parameters() method override causing base model parameters to be optimized

algo/models.py
@@ -11,7 +11,7 @@ import torch.nn.functional as F
 
 from utils import LossBuffer
 
-torch.manual_seed(149)
+torch.manual_seed(135)
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device(
     "cpu")
 
@@ -21,7 +21,7 @@ class ConvModule(nn.Module):
     Provides thes shared convolutional base for the inverse and forward model.
     """
     def __init__(self):
-        super(ConvModule, self).__init__()
+        super().__init__()
         self.conv1 = nn.Conv2d(1, 32, 3, stride=2, padding=1)
         self.conv2 = nn.Conv2d(32, 32, 3, stride=2, padding=1)
         self.conv3 = nn.Conv2d(32, 32, 3, stride=2, padding=1)
@@ -46,14 +46,17 @@ class FCModule(nn.Module):
     Docstring: todo
     """
     def __init__(self, state_dim, embedding_size):
-        super(FCModule, self).__init__()
+        super().__init__()
         self.fc1 = nn.Linear(state_dim, 128)
         self.fc2 = nn.Linear(128, embedding_size)
+        self.bnorm1 = nn.BatchNorm1d(128)
+        self.bnorm2 = nn.BatchNorm1d(embedding_size)
+        self.eval()
 
     def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        return x
+        x = self.bnorm1(F.relu(self.fc1(x)).unsqueeze(0))
+        x = self.bnorm2(F.relu(self.fc2(x)))
+        return x.squeeze()
 
 
 class ForwardModule(nn.Module):
@@ -61,7 +64,7 @@ class ForwardModule(nn.Module):
     Module for learning the forward mapping of state x action -> next state
     """
     def __init__(self, embedding_size, action_dim, base):
-        super(ForwardModule, self).__init__()
+        super().__init__()
         self.base = base
         # we add + 1 because of the concatenated action
         self.l1 = nn.Linear(embedding_size + action_dim, 128)
@@ -83,11 +86,11 @@ class InverseModule(nn.Module):
     Module for learning the inverse mapping of state x next state -> action.
     """
     def __init__(self, embedding_size, action_dim, base):
-        super(InverseModule, self).__init__()
+        super().__init__()
         self.base = base
         # * 2 because we concatenate two states
-        self.linear = nn.Linear(embedding_size * 2, 1024)
-        self.head = nn.Linear(1024, action_dim)
+        self.linear = nn.Linear(embedding_size * 2, 256)
+        self.head = nn.Linear(256, action_dim)
 
     def forward(self, x, y):
         x = self.base(x)
@@ -102,20 +105,18 @@ class ICModule(nn.Module):
     """
     Intrinsic curiosity module.
     """
-    def __init__(self, action_dim, state_dim, embedding_size):
-        super(ICModule, self).__init__()
+    def __init__(self, action_dim, state_dim, embedding_size, alpha=0.5):
+        super().__init__()
         # self._conv_base = ConvModule()
         self.base = FCModule(state_dim, embedding_size)
         # define forward and inverse modules
         self._inverse = InverseModule(embedding_size, action_dim, self.base)
         self._forward = ForwardModule(embedding_size, action_dim, self.base)
 
-        self.opt = optim.RMSprop(self.parameters(),
-                                 lr=1e-4,
-                                 alpha=0.9,
-                                 centered=True)
-
+        self.opt = optim.Adam(self.parameters(), lr=5e-5)
         self.loss_buffer = LossBuffer(100)
+        self.running_return_std = None
+        self.alpha = alpha
 
     def forward(self, x):
         raise NotImplementedError
@@ -126,7 +127,11 @@ class ICModule(nn.Module):
         from base network.
         """
         return set(
-            chain(self._inverse.parameters(), self._forward.parameters()))
+            chain(self._inverse.linear.parameters(),
+                  self._inverse.head.parameters(),
+                  self._forward.l1.parameters(),
+                  self._forward.l2.parameters(),
+                  self._forward.head.parameters()))
 
     def embed(self, state):
         """
@@ -161,8 +166,13 @@ class ICModule(nn.Module):
 
     def _process_loss(self, loss):
         self.loss_buffer.push(loss)
-        runinng_std = self.loss_buffer.get_std()
-        return loss / (runinng_std + 1e-5)
+        return_std = self.loss_buffer.get_std()
+        if self.running_return_std is not None:
+            self.running_return_std = self.alpha * return_std + (
+                1 - self.alpha) * self.running_return_std
+        else:
+            self.running_return_std = return_std
+        return loss / self.running_return_std
 
 
 class MultiModalModule(nn.Module):
@@ -262,8 +272,8 @@ class MMAE(nn.Module):
 
     def forward(self, this_state, action):
         action = torch.stack(action).float().to(device)
-        this_state = list(map(lambda x: torch.cat([x, action], dim=1),
-                              this_state))
+        this_state = list(
+            map(lambda x: torch.cat([x, action], dim=1), this_state))
         prop = self.prop_encoder(this_state[0])
         tac = self.tac_encoder(this_state[1])
         audio = self.audio_encoder(this_state[2])

algo/ppo_cont.py
@@ -80,7 +80,7 @@ class ActorCritic(nn.Module):
         memory.actions.append(action)
         memory.logprobs.append(action_logprob)
 
-        action += torch.randn(self.action_dim) * 0.2
+        action += torch.randn(self.action_dim) * 0.3
 
         return action.detach(), action_mean
 
@@ -110,7 +110,9 @@ class PPO:
 
         self.policy = ActorCritic(action_dim, observation_dim, n_latent_var,
                                   max_action, action_std).to(device)
-        self.optimizer = torch.optim.Adadelta(self.policy.parameters(), lr=lr)
+        self.optimizer = torch.optim.Adam(self.policy.parameters(),
+                                          lr=lr,
+                                          betas=betas)
         self.policy_old = ActorCritic(action_dim, observation_dim,
                                       n_latent_var, max_action,
                                       action_std).to(device)
@@ -134,7 +136,7 @@ class PPO:
 
         # Normalizing the rewards:
         rewards = torch.tensor(rewards).to(device)
-        rewards = (rewards - rewards.mean()) / (rewards.std() + 1e-5)
+        # rewards = (rewards - rewards.mean()) / (rewards.std() + 1e-5)
 
         # convert list to tensor
         old_states = torch.stack(memory.states).to(device).detach()
@@ -152,12 +154,14 @@ class PPO:
 
             # Finding Surrogate Loss:
             advantages = rewards - state_values.detach()
+            advantages = (advantages - advantages.mean()) / (advantages.std() +
+                                                             1e-5)
             surr1 = ratios * advantages
             surr2 = torch.clamp(ratios, 1 - self.eps_clip,
                                 1 + self.eps_clip) * advantages
             value_loss = self.MseLoss(state_values, rewards)
             loss = -torch.min(surr1,
-                              surr2) + 0.5 * value_loss - 0.25 * dist_entropy
+                              surr2) + 0.5 * value_loss - 0.35 * dist_entropy
 
             # take gradient step
             self.optimizer.zero_grad()

env/environment.py
@@ -1,5 +1,6 @@
 import os
 import gym
+import math
 import numpy as np
 from pyrep import PyRep
 from pyrep.objects.force_sensor import ForceSensor
@@ -106,7 +107,7 @@ class Env(gym.Env):
             [self._fs0, self._fs1, self._fs2, self._fs3, self._fs4]
         ]
 
-    def get_sound_signal(self, threshold=0.035):
+    def get_sound_signal(self, threshold=0.025):
         # threshold = 0.035
         collided = self.check_collision()
         if not collided or self.gripper_speed < threshold:
@@ -143,6 +144,9 @@ class Env(gym.Env):
         head_pos = np.array(self.head.get_position())
         radius = np.linalg.norm(gripper_pos - head_pos)
         theta = np.arccos(gripper_pos[2] / radius)
+        if math.isnan(theta):
+            print("Theta set to 0")
+            theta = 0
         phi = np.arctan2(gripper_pos[1], gripper_pos[0])
         return np.array([self.gripper_speed * 100, radius, theta, phi])
 

test.py
@@ -8,9 +8,12 @@ from utils import MMBuffer, GraphWindow
 import pickle
 from collections import namedtuple
 from torch.utils.tensorboard import SummaryWriter
+from utils import SkipWrapper
 
 cnf = get_conf("conf/cnt_col.yaml")
 env = Env(cnf)
+# skip_wrapper = SkipWrapper(1)
+# env = skip_wrapper(env)
 action_dim = env.action_space.shape[0]
 action_dim = cnf.main.action_dim
 state_dim = env.observation_space.shape[0]
@@ -36,7 +39,7 @@ while True:
     im_loss_processed = icmodule._process_loss(im_loss)
     memory.is_terminals.append(done)
     memory.rewards.append(im_loss_processed)
-    if timestep % 100 == 0:
+    if timestep % cnf.main.train_each == 0:
         value = agent.policy_old.critic(memory.states[-1])
         agent.update(memory)
         memory.clear_memory()
@@ -44,8 +47,10 @@ while True:
 
     writer.add_scalar("reward", im_loss_processed, i)
     writer.add_scalar("reward raw", im_loss, i)
-    writer.add_scalar("return std", icmodule.loss_buffer.get_std(), i)
+    writer.add_scalar("return std", icmodule.running_return_std, i)
     writer.add_scalar("value fn", value, i)
-    writer.add_histogram("action mean", action_mean, i)
+    # for key, value in icmodule.base.named_parameters():
+    #     writer.add_histogram(key, value, i)
+    # writer.add_histogram("action mean", action_mean, i)
     # win.update(im_loss_processed, im_loss, icmodule.loss_buffer.get_std(),
     #            value)