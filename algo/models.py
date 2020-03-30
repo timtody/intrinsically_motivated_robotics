2ebe24c726660502e456bd24925987db13c095b9
Julius Taylor <taylor@fias.uni-frankfurt.de>
2020-03-30 18:58:53 +0200

    Add load/save to ICM

--- algo/models.py
+++ algo/models.py
@@ -191,10 +191,10 @@ class ICModule(nn.Module):
             self.running_return_std = return_std
         return loss / self.running_return_std
 
-    def save(self, path):
-        torch.save(self.state_dict(), f"{path}_{icm}.pt")
+    def save(self, timestep):
+        torch.save(
+            self.state_dict(), os.path.join("checkpoints", str(timestep), "icm.pt")
+        )
 
     def load(self, path):
-        self.load_state_dict(
-            torch.load(os.join(os.path.abspath(path), "models", "icm.pt"))
-        )
+        self.load_state_dict(torch.load(os.join(os.path.abspath(path), "icm.pt")))
