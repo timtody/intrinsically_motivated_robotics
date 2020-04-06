5ab5d85d908a24ea9c163a60395e38d14828ee29
Julius Taylor <taylor@fias.uni-frankfurt.de>
2020-04-06 18:13:37 +0200

    Tinker with testing script

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 from experiment import GoalReach
 from utils import get_conf
-from logging import Logger
+from logger import Logger
 
 cnf = get_conf("conf/main.yaml")
 Logger(cnf)
-exp = GoalReachAgent(cnf, 0)
+exp = GoalReach(cnf, 0)
 exp.run()
