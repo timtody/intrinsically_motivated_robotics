ade7e161240993370e73ee5e8ac66c6572e5208c
Julius Taylor <taylor@fias.uni-frankfurt.de>
2020-04-09 12:56:36 +0200

    Add some doc to agent

--- agent.py
+++ agent.py
@@ -1,3 +1,8 @@
+"""
+This file unifies functionality for the PPO agent using the intrinsic curiosity module.
+It's supposed to be used to quantitatively analyze the agent's touching behavior
+towards it's environment like the table, itself and a pendulum which is in the scene.
+"""
 import os
 import torch
 import collections
@@ -20,6 +25,11 @@ class Agent:
         self.icm_buffer = []
 
     def append_icm_transition(self, this_state, next_state, action) -> None:
+        """
+        This is for batched training of the ICM. ICM transitions are needed
+        for computing the intrinsic reward which is the error when predicting
+        s_t+1 from (s_t, a).
+        """
         self.icm_buffer.append(self.icm_transition(this_state, next_state, action))
 
     def set_is_done(self, is_done) -> None:
@@ -33,8 +43,10 @@ class Agent:
         return action
 
     def train(self) -> dict:
-        # TODO: return results dict
-        # train icm
+        """
+        Trains the ICM of the agent. This method clears the buffer which was filled by
+        this.append_icm_transition
+        """
         state_batch, next_state_batch, action_batch = zip(*self.icm_buffer)
         im_loss_batch = self.icm.train_forward(
             state_batch, next_state_batch, action_batch
