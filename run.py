777f46e0418e162ee04c4f0bdd8b02a37ff5d417
Julius Taylor <taylor@fias.uni-frankfurt.de>
2020-05-02 18:35:09 +0200

    Added guard to run.py

--- run.py
+++ run.py
@@ -1,3 +1,17 @@
+import os
+
 from experiment import TestFWModel
 import multiprocessing as mp
 from runner import Runner
+from utils import get_conf
+
+if __name__ == "__main__":
+
+    # os.environ["WANDB_MODE"] = "dryrun"
+    # os.environ["WANDB_DISABLE_CODE"] = "true"
+
+    mp.set_start_method("spawn")
+
+    cnf = get_conf("conf/main.yaml")
+    runner = Runner(TestFWModel, cnf)
+    runner.run()
