f6108ae3a148b629f5bf41fa97e4ce7417d47328
Julius Taylor <taylor@fias.uni-frankfurt.de>
2020-05-08 18:11:24 +0200

    Add mpirunner

--- run_dp.py
+++ run_dp.py
@@ -1,23 +1,30 @@
 import os
 import sys
-import ray
 import importlib
 import multiprocessing as mp
 from exp_runner import Runner
 from utils import get_conf
 
-if __name__ == "__main__":
+from mpi4py import MPI
 
-    mp.set_start_method("spawn")
 
-    # dynamicall import the experiment
-    experiment = sys.argv[1].split("/")[1].split(".")[0]
-    module = importlib.import_module("." + experiment, package="experiments")
-    exp = getattr(module, "Experiment")
+nproc = MPI.COMM_WORLD.Get_size()  # Size of communicator
+iproc = MPI.COMM_WORLD.Get_rank()  # Ranks in communicator
+inode = MPI.Get_processor_name()  # Node where this MPI process runs
 
-    cnf = get_conf("conf/main.yaml")
-    if cnf.wandb.dryrun:
-        os.environ["WANDB_MODE"] = "dryrun"
-        os.environ["WANDB_DISABLE_CODE"] = "true"
-    runner = Runner(exp, cnf)
-    runner.run()
+
+# dynamicall import the experiment
+experiment = sys.argv[1].split("/")[1].split(".")[0]
+module = importlib.import_module("." + experiment, package="experiments")
+exp = getattr(module, "Experiment")
+
+cnf = get_conf("conf/main.yaml")
+
+if cnf.wandb.dryrun:
+    os.environ["WANDB_MODE"] = "dryrun"
+    os.environ["WANDB_DISABLE_CODE"] = "true"
+
+experiment = exp(cnf, iproc)
+experiment.run()
+
+MPI.Finalize()
