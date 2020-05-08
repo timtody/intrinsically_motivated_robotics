5e9c60ce10106919c4a7ca01297d2b5193cad443
Julius Taylor <taylor@fias.uni-frankfurt.de>
2020-05-08 16:39:43 +0200

    Bruh

--- test.py
+++ test.py
@@ -22,37 +22,16 @@ import multiprocessing as mp
 
 class Exp(Experiment):
     def run(self):
-        sensors = self.env.skin_wrist
-        self.win = GraphWindow(
-            [
-                label
-                for tup in [
-                    (f"sensor_{i}_x", f"sensor_{i}_y", f"sensor_{i}_z")
-                    for i in range(len(sensors))
-                ]
-                for label in tup
-            ],
-            3,
-            4,
-            autoscale=False,
-        )
+        obs = self.env.reset()
+        print(len(obs))
+        exit(1)
+        for i in range(15):
+            self.env.step([0, 0, 0, 0, 0, 1, 0])
 
-        start = time.time()
-        for i in range(10000):
-            # input()
-            # obs, *_ = self.env.step([0, 0, 0, 0, 0, -1, -0.3])
-            obs, *_ = self.env.step(self.env.action_space.sample())
-            readings = [e for skin_sensor in sensors for e in skin_sensor.read()[0]]
-            self.win.update(*readings)
-            # print(obs)
-            # print(len(obs))
-            # exit(1)
-            self_collision = self.env.check_collision_with_self()
-            other_collision = self.env.check_collision()
-            dynamic_collision = self.env.check_collision_with_dynamic()
+        for i in range(200):
+            self.env.step([0, 1, 0, 0, 0, 0, 0])
 
-        end = time.time()
-        print("took", end - start, "s")
+        self.env.close()
 
 
 if __name__ == "__main__":
@@ -66,5 +45,5 @@ if __name__ == "__main__":
 
     cnf = get_conf("conf/main.yaml")
     # TODO: remove this when loading forgetting db
-    runner = Runner(GoalBasedExp, cnf)
-    runner.run()
+    exp = GoalBasedExp(cnf, 0)
+    exp.run()
