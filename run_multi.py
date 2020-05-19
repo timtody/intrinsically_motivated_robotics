a9f5bd1134ec99179163c26d52ee04295ab96f15
Julius Taylor <taylor@fias.uni-frankfurt.de>
2020-05-19 12:03:29 +0200

    Add run_multi.py sweeper

---
+++ run_multi.py
@@ -0,0 +1,27 @@
+import os
+import sys
+import importlib
+import multiprocessing as mp
+from mp_runner import Runner
+from utils import get_conf
+
+if __name__ == "__main__":
+
+    mp.set_start_method("spawn")
+
+    # dynamicall import the experiment
+    experiment = sys.argv[1].split("/")[1].split(".")[0]
+    module = importlib.import_module("." + experiment, package="experiments")
+    exp = getattr(module, "Experiment")
+
+    cnf = get_conf("conf/main.yaml")
+    if cnf.wandb.dryrun:
+        os.environ["WANDB_MODE"] = "dryrun"
+        os.environ["WANDB_DISABLE_CODE"] = "true"
+
+    modes = ["tac", "prop", "tac,prop", "tac,prop,mobile"]
+    for mode in modes:
+        print("starting mode", mode)
+        cnf.env.state = mode
+        runner = Runner(exp, cnf)
+        runner.run()
