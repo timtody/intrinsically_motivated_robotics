35c21fb79e78d5137248450ab4c1c2832857a8c4
Julius Taylor <taylor@fias.uni-frankfurt.de>
2020-05-28 17:08:13 +0200

    Tweak conf parameters

--- jobscript.sh
+++ jobscript.sh
@@ -1,15 +1,1 @@
-#!/bin/bash
-#SBATCH --job-name=foo
-#SBATCH --partition=fuchs
-#SBATCH --nodes=1
-#SBATCH --ntasks=20
-#SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=1000
-#SBATCH --time=21-0
-#SBATCH --no-requeue
-#SBATCH --mail-type=FAIL
-#SBATCH –-extra-node-info=2:10:1
-
-python run.py experiments/count_collisions.py mp.n_procs=10 wandb.dryrun=False wandb.project=collisions-new wandb.name=test-sbatch &
-python run.py experiments/count_collisions.py mp.n_procs=10 wandb.dryrun=False wandb.project=collisions-new wandb.name=test-sbatch &
-wait
+mpiexec xvfb-run -a -s "-screen 0 640x480x16"
